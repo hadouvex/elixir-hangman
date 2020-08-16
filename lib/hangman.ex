08dfcd1bbff0974b1a648820019c869bd0df740b
hadouvex <undercover.agents.spy@gmail.com>
2020-08-16 20:30:36 +0300

    Minor fix

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -1,8 +1,8 @@
 defmodule Hangman do
-  # @words ['cat', 'dog', 'human', 'house', 'airplane', 'replication', 'terraformation']
-  @words ['human']
+  @words ['cat', 'dog', 'human', 'house', 'airplane', 'replication', 'terraformation']
+  # @words ['human']
   # @numerals ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-   @numerals '0123456789'
+  @numerals '0123456789'
 
   @moduledoc """
   Documentation for `Hangman`.
